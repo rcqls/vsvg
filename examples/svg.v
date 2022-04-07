2982e4a728cbb5879aeefd15aa10ee50827e92fa
R cqls <rdrouilh@gmail.com>
2022-04-07 05:27:33 +0200

    add resize

--- examples/svg.v
+++ examples/svg.v
@@ -3,12 +3,12 @@ import vsvg
 mut s := vsvg.svg(width: 192, height: 320)
 
 s.begin()
-s.fill("#DADAFF")
+s.fill('#DADAFF')
 // s.text(32, 32, "draw all shapes", "#000000", "#000000", font_name: "sans-serif", size: 16)
-s.circle(32, 64, 96, stroke: "#000080", strokewidth: 4, fill: "#0000FF")
-s.ellipse(64, 160, 32, 16, fill: "#FF0000", stroke: "#800000", strokewidth: 4)
-s.line(32, 192, 160, 192, stroke: "#000000", strokewidth: 2)
-s.rectangle(64, 64, 32, 224, rx: 4, ry: 4, fill: "#00FF00", stroke: "#008000", strokewidth: 4)
+s.circle(32, 64, 96, stroke: '#000080', strokewidth: 4, fill: '#0000FF')
+s.ellipse(64, 160, 32, 16, fill: '#FF0000', stroke: '#800000', strokewidth: 4)
+s.line(32, 192, 160, 192, stroke: '#000000', strokewidth: 2)
+s.rectangle(64, 64, 32, 224, rx: 4, ry: 4, fill: '#00FF00', stroke: '#008000', strokewidth: 4)
 s.end()
 
-s.save("allshapes.svg") ?
+s.save('allshapes.svg') ?
--- svg.v
+++ svg.v
@@ -5,17 +5,17 @@ import os
 
 struct Svg {
 pub:
-	height int
-	width int
 	buffer strings.Builder = strings.new_builder(32768)
 pub mut:
+	height  int
+	width   int
 	content &strings.Builder = 0
 }
 
 [params]
 pub struct SvgParams {
 	height int
-	width int
+	width  int
 }
 
 pub fn svg(p SvgParams) &Svg {
@@ -27,12 +27,16 @@ pub fn svg(p SvgParams) &Svg {
 	return s
 }
 
-pub fn(mut s Svg) begin() {
+pub fn (mut s Svg) resize(w int, h int) {
+	s.width, s.height = w, h
+}
+
+pub fn (mut s Svg) begin() {
 	s.content.write_string("<?xml version='1.0' encoding='utf-8'?>\n<svg width='${s.width}px' height='${s.height}px'  xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink'>\n")
 }
 
-pub fn(mut s Svg) end() {
-	s.content.write_string("</svg>\n")
+pub fn (mut s Svg) end() {
+	s.content.write_string('</svg>\n')
 }
 
 pub fn (mut s Svg) save(filepath string) ? {
@@ -42,45 +46,45 @@ pub fn (mut s Svg) save(filepath string) ? {
 
 [params]
 pub struct Params {
-	stroke string = "none"
+	stroke      string = 'none'
 	strokewidth int
-	fill string = "none"
-	rx int
-	ry int
+	fill        string = 'none'
+	rx          int
+	ry          int
 }
 
 pub fn (mut s Svg) circle(x int, y int, r int, p Params) {
-	s.content.write_string("<circle  cy='${y}' cx='${x}' r='${r}'  stroke='${p.stroke}' stroke-width='${p.strokewidth}px' fill='${p.fill}' />\n")
+	s.content.write_string("<circle  cy='$y' cx='$x' r='$r'  stroke='$p.stroke' stroke-width='${p.strokewidth}px' fill='$p.fill' />\n")
 }
 
 pub fn (mut s Svg) line(x1 int, y1 int, x2 int, y2 int, p Params) {
-	s.content.write_string("<line x1='${x1}' y1='${y1}' x2='${x2}' y2='${y2}' stroke='${p.stroke}' stroke-width='${p.strokewidth}px' />\n")
+	s.content.write_string("<line x1='$x1' y1='$y1' x2='$x2' y2='$y2' stroke='$p.stroke' stroke-width='${p.strokewidth}px' />\n")
 }
 
 pub fn (mut s Svg) rectangle(x int, y int, width int, height int, p Params) {
-	s.content.write_string("<rect x='${x}' y='${y}' width='${width}' height='${height}' rx='${p.rx}' ry='${p.ry}' fill='${p.fill}' stroke='${p.stroke}' stroke-width='${p.strokewidth}px' />\n")
+	s.content.write_string("<rect x='$x' y='$y' width='$width' height='$height' rx='$p.rx' ry='$p.ry' fill='$p.fill' stroke='$p.stroke' stroke-width='${p.strokewidth}px' />\n")
 }
 
 pub fn (mut s Svg) fill(fill string) {
-    s.rectangle(0, 0, s.width, s.height, fill: fill)
+	s.rectangle(0, 0, s.width, s.height, fill: fill)
 }
 
-pub fn (mut s Svg) text(x int, y int,  text string, fill string, ts TextStyle) {
-	s.content.write_string("<text x='${x}' y='${y}' fill='${fill}' stroke='${vsvg.color(ts.color)}' font-family='${ts.font_name}' font-size='${ts.size}px' dominant-baseline='${ts.vertical_align}' text-anchor='${ts.align}'><![CDATA[${text}]]></text>\n")
+pub fn (mut s Svg) text(x int, y int, text string, fill string, ts TextStyle) {
+	s.content.write_string("<text x='$x' y='$y' fill='$fill' stroke='${color(ts.color)}' font-family='$ts.font_name' font-size='${ts.size}px' dominant-baseline='$ts.vertical_align' text-anchor='$ts.align'><![CDATA[$text]]></text>\n")
 }
 
 pub fn (mut s Svg) ellipse(x int, y int, rx int, ry int, p Params) {
-	s.content.write_string("<ellipse cx='${x}' cy='${y}' rx='${rx}' ry='${ry}' fill='${p.fill}' stroke='${p.stroke}' stroke-width='${p.strokewidth}px' />\n")
+	s.content.write_string("<ellipse cx='$x' cy='$y' rx='$rx' ry='$ry' fill='$p.fill' stroke='$p.stroke' stroke-width='${p.strokewidth}px' />\n")
 }
 
 pub fn (mut s Svg) polygon(points string, p Params) {
-	s.content.write_string("<polygon points='${points}' fill='${p.fill}' stroke='${p.stroke}' stroke-width='${p.strokewidth}px' />\n")
+	s.content.write_string("<polygon points='$points' fill='$p.fill' stroke='$p.stroke' stroke-width='${p.strokewidth}px' />\n")
 }
 
 pub fn (mut s Svg) polyline(points string, p Params) {
-	s.content.write_string("<polyline points='${points}' fill='${p.fill}' stroke='${p.stroke}' stroke-width='${p.strokewidth}px' />\n")
+	s.content.write_string("<polyline points='$points' fill='$p.fill' stroke='$p.stroke' stroke-width='${p.strokewidth}px' />\n")
 }
 
 pub fn (mut s Svg) image(x int, y int, width int, height int, path string) {
-	s.content.write_string("<image xlink:href='${path}' x='${x}' y='${y}' height='${height}' width='${width}' />\n")
+	s.content.write_string("<image xlink:href='$path' x='$x' y='$y' height='$height' width='$width' />\n")
 }
